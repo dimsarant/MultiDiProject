458e3c2e78ed0d743cc3a9236cacbbab1dea4a47
BalrokHS <kostas.lianos.96@gmail.com>
2019-11-17 01:08:28 +0200

    Beautify shingling.py

--- src/shingling.py
+++ src/shingling.py
@@ -1,14 +1,20 @@
 from functools import reduce
 from hashlib import md5
 
+
 def shingles(text):
-    if len(text) > 1000: return get_shingles(text, 10)
+    if len(text) > 1000:
+        return get_shingles(text, 10)
     return get_shingles(text, 5)
 
+
 def get_shingles(text, k):
-    list = [md5(text[i:i+k].encode('ascii')).hexdigest() for i in range(len(text) - k)]
-    unique_list = reduce(lambda x,y: x+[y] if x==[] or x[-1] != y else x, list,[])
+    list = [md5(text[i:i + k].encode('ascii')).hexdigest()
+            for i in range(len(text) - k)]
+    unique_list = reduce(
+        lambda x, y: x + [y] if x == [] or x[-1] != y else x, list, [])
     return unique_list
+
 
 if __name__ == "__main__":
     test = shingles("dhjsakdhjksladh aaaah hdhh dhisis bb b sbbbbbbbbbbbds")
