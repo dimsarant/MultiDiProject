2b0e5b0af36c1abef4e8807ba03b0912df6fa834
BalrokHS <kostas.lianos.96@gmail.com>
2019-12-15 19:03:49 +0200

    Discarded Panda Module, and fixed the NaN error

--- main.py
+++ main.py
@@ -1,37 +1,39 @@
 import os
 import sys
 import numpy as np
-import pandas as pd
-from src.lsh import *
-from src.shingling import *
-from src.minhash import *
-from src.cosim import *
+from src.lsh import filter_buckets, calculate_similarity, combinations
+from src.shingling import getSinglingMatrix
+from src.minhash import getSignatures
+from src.cosim import cosign_sim
 
 
-def select_candidates(probabilities):
-    candidates = pd.DataFrame([list(range(len(text_list))),list(probabilities)], index=['Combinations', 'Probabilities'])
-    candidates = candidates.sort_values(by='Probabilities', axis=1, ascending=False)
-    candidates = list(candidates.iloc[0])
-    return candidates
+# def select_candidates(probabilities):
+#     candidates = pd.DataFrame([list(range(len(text_list))), list(probabilities)],
+#                               index=['Combinations', 'Probabilities'])
+#     candidates = candidates.sort_values(by='Probabilities', axis=1, ascending=False)
+#     candidates = list(candidates.iloc[0])
+#     return candidates
 
 
 if __name__ == "__main__":
-    text_list = []
-    test = "aaaaa bbbbb ccccc"
-    test_new = "aaaaa bbbbb ccccc"
-    test_test = "bbbbb ccccc ddddd"
 
-    text_list.append(test)
-    text_list.append(test_new)
-    text_list.append(test_test)
-    similarities=np.array(calculate_similarity(filter_buckets(getSignatures(getSinglingMatrix(text_list), 100)),len(text_list)))
-    probabilities=similarities[np.triu_indices(len(similarities),1)]
-    print("\nBucket Results:")
-    for index, comb in enumerate(combinations(range(len(text_list)), 2)):
-        print("Texts "+str(comb)+" : similarity = "+str(probabilities[index]))
+    text_list = []
+    for file in os.listdir("data/"):
+        if file.endswith(".txt"):
+            filename = os.path.join("data", file)
+
+            f = open(filename, 'r')
+            text_list.append(f.read())
+
+    similarities = np.array(calculate_similarity(filter_buckets(
+        getSignatures(getSinglingMatrix(text_list), 100)), len(text_list)))
+
+    triu_idx = combinations(range(len(text_list)), 2)
 
     print("\nSimilarity Results:")
-    similarity=cosign_sim(text_list)
-    combinations = list(combinations(range(len(text_list)), 2))
-    for candidate in select_candidates(probabilities):
-        print("Texts "+str(combinations[candidate])+" : similarity = "+str(similarity[candidate])+"%")
+    for i, j in triu_idx:
+        if similarities[i][j] != 0:
+            # print(similarities[i][j]])
+            similarity = cosign_sim([text_list[i], text_list[j]])
+            # combinations = list(combinations(range(len(text_list)), 2))
+            print("Texts ({},{}) : similarity = {}%".format(i, j, similarity))
